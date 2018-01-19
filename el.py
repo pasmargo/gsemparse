687441260a4ee583f45e77c648b987605ce6b7bc
Pascual <pasmargo@gmail.com>
2018-01-19 17:42:14 +0900

    Sort partition results of entity linking

--- el.py
+++ el.py
@@ -27,24 +27,13 @@ if __name__ == '__main__':
         help="Filename where the Keras encoder is saved.")
     parser.add_argument("--model_type", nargs='?', default="char-cnn",
         choices=["char-cnn", "char-lstm"])
-    parser.add_argument("--loss_type", nargs='?', default="i1i1",
-        choices=["i1i1", "i1i1-i1i2s", "i1i1-i1i2s-i1j1s"])
     parser.add_argument("--nlabels", nargs='?', type=int, default=-1,
         help="Number of entities to load from file. If -1, then load them all.")
     parser.add_argument("--maxlen", nargs='?', type=int, default=16,
         help="Maximum length of labels. Longer labels are cropped.")
     parser.add_argument("--fields", nargs='?', default="uri,label")
     parser.add_argument("--char_emb_size", nargs='?', type=int, default=128)
-    parser.add_argument("--batch_size", nargs='?', type=int, default=500)
-    parser.add_argument("--batch_size_val", nargs='?', type=int, default=150)
-    parser.add_argument("--epochs", nargs='?', type=int, default=200)
-    parser.add_argument("--steps_per_epoch", nargs='?', type=int, default=100)
-    parser.add_argument("--validation_steps", nargs='?', type=int, default=1)
-    parser.add_argument("--patience", nargs='?', type=int, default=5)
-    parser.add_argument("--batch_normalization", nargs='?', type=bool, default=False)
-    parser.add_argument("--reduce_params", dest="reduce_params", action="store_true")
-    parser.add_argument("--no_reduce_params", dest="reduce_params", action="store_false")
-    parser.add_argument("--exp_suffix", nargs='?', default="")
+    parser.add_argument("--batch_size", nargs='?', type=int, default=1000)
     parser.set_defaults(reduce_params=True)
     args = parser.parse_args()
 
@@ -56,11 +45,13 @@ if __name__ == '__main__':
         parser.print_help(file=sys.stderr)
         sys.exit(1)
 
+    logging.info('Loading KB entities...')
     labels, entities = load_labels(
         'data/dbpedia_ents.text.jsonl',
         ntrain=args.nlabels,
         return_jsonl=True)
     assert len(labels) == len(entities)
+    logging.info('Finished load.')
     X = labels_to_matrix(labels, args.maxlen)
 
     mentions = [m.strip().lower() for m in args.queries]
@@ -102,11 +93,15 @@ if __name__ == '__main__':
 
     diffs_argpart = np.argpartition(diffs, args.nbest)
     best_entries = list(diffs_argpart[0][:args.nbest])
+    best_entities = []
     for i in best_entries:
         entity = entities[i]
         for field in list(entity.keys()):
             if field not in fields:
                 del entity[field]
             entity['score'] = 1 - diffs[0][i]
+        best_entities.append(entity)
+    best_entities.sort(key=lambda e: e['score'], reverse=True)
+    for entity in best_entities:
         print(json.dumps(entity, indent=None))
 
